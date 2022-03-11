4759e0a5d1bd470532c9964cee985a71aeb6ab09
mdsajedul <sajedulislms@gmail.com>
2022-03-12 02:40:04 +0600

    error handler half done

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
  const express = require('express');
  const dotenv = require('dotenv');
  const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+// internal imports 
+const {notFoundHandler,errorHandler} = require('./middlewares/common/errorHandler')
  
  const app = express();
  dotenv.config();
@@ -14,8 +18,16 @@
 
  app.use(express.json());
  app.use(express.urlencoded({extended:true}));
+ app.use(cookieParser(process.env.COOKIE_SECRET))
 
  const port = process.env.PORT || 3000;
+
+
+//404 not found handler
+app.use(notFoundHandler)
+//default error handler 
+app.use(errorHandler)
+
 
  app.listen(port,()=>{
      console.log(`Server is running on PORT ${port}`);
---
+++ middlewares/common/errorHandler.js
@@ -0,0 +1,17 @@
+const createError = require('http-errors')
+
+// 404 not found 
+function notFoundHandler(req,res,next){
+    next(createError(404,'Your requested content was not found'));
+}
+
+// default error handler 
+
+function errorHandler(err,req,res,next){
+    res.status(500).json({'error':'There is an error'});
+}
+
+module.exports = {
+    notFoundHandler,
+    errorHandler
+}
